e238bf805585d65b4c43aba2444dd1d62ca62685
conto <constantijn@vanhartesveldt.nl>
2021-04-23 00:09:36 +0200

    bijna klaar met front end

--- categories.php
+++ categories.php
@@ -2,12 +2,20 @@
 include('./core/header.php');
 ?>
 <main>
-<link rel="stylesheet" href="./assets/css/style.css"> <!-- FIXME add prefetch -->
-<link rel="stylesheet" href="./assets/css/categories.css">
-<div class="categorie_card"></div>
-<div class="categorie_card"></div>
-<div class="categorie_card"></div>
-<div class="categorie_card"></div>
+    <link rel="stylesheet" href="./assets/css/style.css"> <!-- FIXME add prefetch -->
+    <link rel="stylesheet" href="./assets/css/categories.css">
+    <a href="#">
+        <figure class="categorie_card">
+            <img height="100%" width="100%" src="https://i.redd.it/niuqy8iep3p21.jpg" alt="catogorie photo">
+        </figure>
+        <figcaption class="categorie_card_info">
+    this is a big busty
+        </figcaption>
+        
+    </a>
+
+
+
 </main>
 <?php
 include('core/footer.php');
---
+++ product.php
@@ -0,0 +1,57 @@
+<?php
+include('./core/header.php');
+?>
+<main>
+    <link rel="stylesheet" href="./assets/css/style.css"> <!-- FIXME add prefetch -->
+    <link rel="stylesheet" href="./assets/css/product.css">
+
+    <img class="product_photo" src="https://i.redd.it/soqmyvvu68u61.png" alt="product">
+    <article class="product_article">
+        I like my women like I like my coffee.
+        Ground up and put in the freezer.
+        I am from Kenya, please send help
+        I miss my dad
+        if i was anymore inbred id be a sandwich
+        i am the supreme gentlemen
+        fuck her right in the pussy
+        my dad left me
+        this beat is going down like a south korean ferry
+        What's better than swinging a dead baby in circles around your head on a 5 foot rope? Stopping it with a shovel.
+        there are orphans shitting in my mouth
+        i cry a lot when i touch myself
+        d d d d dad whats of breakfast
+        the jews did 911
+        filthy frank fans will never get girlfriends and will die alone
+        whats better than a pile of dead babies? one in the middle is alive and has to eat it's way out
+        <br>
+        what the fuck did you just say about me, you little bitch?ill have you know i graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills etc
+        kekekekekekekek jajajajajajajajja a good 20% of the comments
+        cory in the house
+        dead girls can't say no
+        sometimes i sit in a deep dark corner regretting the day i didn't use a condom on rosie odonnel
+        in the 8th grade a guy almost put an egg up his ass because of me
+        how do you make a baby cry? drop it. how do you make it stop crying? drop it again.
+        falcon punching pregnant women is my favorite past time
+        it hurts but i do it for shrek
+        your mom should have swallowed you
+        hey mom I'm in a filthy frank video
+        pink guy sucks he is a gay piece of shit and needs to die
+        this one time i pissed blood and drank it. recycling is good.
+        i want to caress matty b raps and make him squeal like the lil boy he is
+        consensual sex in the missionary position with the lights off
+        what's red, 6 inches long, and makes my girlfriend cry when i feed it to her? her miscarriage
+        if she's old enough to pee then she's old enough for me
+        as long as i have a face your mother has somewhere to sit
+        the rest of the comments were b0ss pls gibe da pusi b0ss
+        <br>
+        tags: so a girl walks into a deli and sees a man in his 40's buying a pack of cigarettes and asks him "excuse me how many packs do you buy a week" and he says "five." the girl then does the math and says "well if you saved all that money instead of buying cigarettes for approximately 8 years, you could have a FERRARI by now... and not be driving that crappy toyota." the man then looks at her and says "well where the fuck is your ferrari top kek kill yourself"
+        mfw no ferrari
+        mfw using green text on youtube
+        mfw using mfw
+        kill yourself
+    </article>
+
+</main>
+<?php
+include('core/footer.php');
+?>
